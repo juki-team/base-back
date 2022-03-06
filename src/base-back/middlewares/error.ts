3727fb299f929e22954e3c1fa81b7b21789f349d
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-03-06 10:01:06 -0400

    v1.0.2

---
+++ src/base-back/middlewares/error.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from 'express';
+import { ERROR } from '../config/constants';
+import { errorsResponse } from '../helpers';
+import { jkLogTelegramBot } from '../services';
+import { ErrorCode, JkError } from '../types';
+
+/*
+ https://github.com/visionmedia/supertest/issues/416
+ 
+ Error-handling middleware always takes four arguments.
+ You must provide four arguments to identify it as an error-handling middleware function.
+ Even if you don’t need to use the next object, you must specify it to maintain the signature.
+ Otherwise, the next object will be interpreted as regular middleware and will fail to handle errors.
+ */
+
+export function errorLogger(err: any, request: Request, response: Response, next: NextFunction) {
+  const { headers, method, url } = request;
+  const error = {
+    headers,
+    method,
+    url,
+    error: err.stack,
+  };
+  jkLogTelegramBot.sendErrorMessage(`Logging error [[${url}]]`, error);
+  next(err);
+}
+
+export function errorResponder(err: any, request: Request, response: Response, next: NextFunction) {
+  if (request.xhr) {
+    response
+      .status(500)
+      .send(errorsResponse(
+        err?.message || ERROR[ErrorCode.ERR500].message,
+        new JkError(ErrorCode.ERR500, { message: err?.message, stack: err?.stack }),
+      ));
+  } else {
+    next(err);
+  }
+}
+
+export function failSafeHandler(err: any, request: Request, response: Response, next: NextFunction) {
+  response
+    .status(500)
+    .send(errorsResponse(
+      err?.message || ERROR[ErrorCode.ERR500].message,
+      new JkError(ErrorCode.ERR500, { message: err?.message, stack: err?.stack }),
+    ));
+}
