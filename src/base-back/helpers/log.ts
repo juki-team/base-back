37106b4aa8db0abcac7efb764230555e9afc9814
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-03-29 13:08:57 -0400

    Fixing title log and upd jk commons

--- src/base-back/helpers/log.ts
+++ src/base-back/helpers/log.ts
@@ -5,14 +5,14 @@ export const logMessage = (message: any) => {
 
 export const logInfo = (content: any, title?: string) => {
   const now = new Date();
-  console.log(`[INFO] ${now}, ${title}`);
+  console.log(`[INFO] ${now}` + (title ? `, ${title}:` : ':'));
   console.log(content);
   console.log(`------`);
 };
 
 export const logError = (content: any, title?: string) => {
   const now = new Date();
-  console.log(`[ERROR] ${now}, ${title}`);
+  console.log(`[ERROR] ${now}` + (title ? `, ${title}:` : ':'));
   console.log(content);
   console.log(`_______`);
 };
