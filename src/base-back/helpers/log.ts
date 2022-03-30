f5dc1cfc884ef2c8eb40db8e92ddb60ad9f32dff
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-03-30 14:28:20 -0400

    Fixing logs

--- src/base-back/helpers/log.ts
+++ src/base-back/helpers/log.ts
@@ -1,3 +1,5 @@
+import util from 'util';
+
 export const logMessage = (message: any) => {
   const now = new Date();
   console.log(`[MSG] ${now}, ${message}`);
@@ -6,13 +8,13 @@ export const logMessage = (message: any) => {
 export const logInfo = (content: any, title?: string) => {
   const now = new Date();
   console.log(`[INFO] ${now}` + (title ? `, ${title}:` : ':'));
-  console.log(content);
+  console.log(util.inspect(content, { depth: 5, compact: false }));
   console.log(`------`);
 };
 
 export const logError = (content: any, title?: string) => {
   const now = new Date();
   console.log(`[ERROR] ${now}` + (title ? `, ${title}:` : ':'));
-  console.log(content);
+  console.log(util.inspect(content, { depth: 5, compact: false }));
   console.log(`_______`);
 };
