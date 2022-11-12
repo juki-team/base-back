d442d13e32b9d7bb319dc728cc07b03efaedf745
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-11-11 20:20:36 -0400

    0.0.51

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 require('dotenv').config();
 import {
   jkLogTelegramBot,
+  NODE_ENV,
   TELEGRAM_JUKI_ERROR_LOGS_CHAT_ID,
   TELEGRAM_JUKI_INFO_LOGS_CHAT_ID,
   TELEGRAM_JUKI_LOGS_BOT_TOKEN,
 } from './index';
-
-export const NODE_ENV = process.env.NODE_ENV || 'development';
 
 jkLogTelegramBot.config(TELEGRAM_JUKI_LOGS_BOT_TOKEN, TELEGRAM_JUKI_INFO_LOGS_CHAT_ID, TELEGRAM_JUKI_ERROR_LOGS_CHAT_ID, '__BASE BACK__');
 
