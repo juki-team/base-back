5063cb0ef85029d198cecb522afe0fc91d6f5f38
Oscar Gauss Carvajal Yucra <oscargausscarvajal@gmail.com>
2022-11-15 22:43:42 -0400

    0.0.58

--- src/services/aws/ecs.ts
+++ src/services/aws/ecs.ts
@@ -17,7 +17,7 @@ export const ecsCluster = (cluster: string) => ({
   listTasks: async () => (
     await awsEcs.listTasks({ cluster }).promise()
   ),
-  describeTasks: async (tasks: string[]) => (
+  describeTasks: async (tasks: AWS.ECS.StringList) => (
     await awsEcs.describeTasks({ cluster, tasks }).promise()
   ),
   stopTask: async (task: string) => (
